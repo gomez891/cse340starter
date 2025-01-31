54f4c9a63eb0dd21fe460be2896e62e070897232
gomez891 <gomezjonathon891@gmail.com>
2025-01-30 20:45:01 -0700

    Week 4 Prepare Part 1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -24,10 +24,8 @@ app.set("layout","./layouts/layout")
  * Routes
  *************************/
 app.use(static)
-// Index rout
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// Index route
+app.get("/", baseController.buildHome)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
