551fc299b0f1479104d0ef1e40c467bc71d7b1a1
gomez891 <gomezjonathon891@gmail.com>
2025-02-02 17:37:01 -0700

    Week 4 Prepart Pt 3

--- controllers/invController.js
+++ controllers/invController.js
@@ -8,16 +8,16 @@ const invCont = {}
  * ************************** */
 
 invCont.buildByClassificationId = async function (req, res, next) {
-    const classification_id = req.params.classification_id
+    const classification_id = req.params.classificationId
     const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildByClassificationId(data)
+    const grid = await utilities.buildClassificationGrid(data)
     let nav = await utilities.getNav()
     const className = data[0].classification_name
     res.render("./inventory/classification", {
-        title: className + " vehicles",
-        nav,
-        grid,
+      title: className + " vehicles",
+      nav,
+      grid,
     })
-}
+  }
 
 module.exports = invCont
