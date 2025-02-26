5d473bcdb96e223e132f7ffe0d2eacd9990365ad
gomez891 <gomezjonathon891@gmail.com>
2025-02-26 14:20:23 -0700

    building registration page

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -9,8 +9,26 @@ async function buildLogin(req, res, next) {
     res.render("account/login", {
         title: "Login",
         nav,
-
+        
     })
 }
 
-module.exports = { buildLogin }
+
+/* ****************************************
+*  Deliver login view
+* *************************************** */
+async function buildRegister(req, res, next) {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+        title: "Register",
+        nav,
+    })
+    
+}
+
+
+
+
+
+
+module.exports = { buildLogin, buildRegister }
--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
 // Account Route
-app.use("/account", accountRoute)
+app.use("/account", require("./routes/accountRoute"))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status:404, message: 'Sorry, we appear to have lost that page'})
