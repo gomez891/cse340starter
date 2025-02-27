bedbfc70a9041d5110ae3e99e2e2cf23ac1e489b
gomez891 <gomezjonathon891@gmail.com>
2025-02-27 01:34:52 -0700

    validating register progamming

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/index")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -47,6 +48,8 @@ app.use(function(req, res, next){
   next()
 })
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 /* ***********************
