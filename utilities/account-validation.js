08393746d37f7b126b9eb56cc6abbeaeb03f21db
gomez891 <gomezjonathon891@gmail.com>
2025-03-04 14:47:53 -0700

    week 8 changes

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -2,6 +2,7 @@ const utilities = require(".")
     const {body, validationResult} = require("express-validator")
     const validate = {}
 
+const accountModel = require("../models/account-model")
 
 /* ************************************
 *   Registration Data Validation Rules
@@ -32,7 +33,13 @@ validate.registrationRules = () => {
         .notEmpty()
         .isEmail()
         .normalizeEmail() // refer to validator.js docs
-        .withMessage("A valid email is required"),
+        .withMessage("A valid email is required")
+        .custom(async (account_email) => {
+            const emailExists = await accountModel.checkExistingEmail(account_email)
+            if(emailExists){
+                throw new Error("Email exists. Please log in or use different email")
+            }
+        }),
 
         // password is required and must be a strong password
         body("account_password")
